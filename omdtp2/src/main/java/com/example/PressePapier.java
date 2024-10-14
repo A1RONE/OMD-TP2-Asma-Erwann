f3ad97b4b402f825972e2f9b8afb734e4164098b
AsmaZd <asmazaied@outlook.fr>
2024-10-14 13:42:27 +0200

    Update PressePapier.java

--- omdtp2/src/main/java/com/example/PressePapier.java
+++ omdtp2/src/main/java/com/example/PressePapier.java
@@ -1,5 +1,26 @@
 package com.example;
 
 public class PressePapier {
-    
+
+    private String m_texte;
+
+    public PressePapier(String texte){
+        m_texte = texte;
+    }
+
+    public PressePapier(){
+        m_texte = "";
+    }
+
+    public void setText(String texte) {
+        m_texte = texte;
+    }
+
+    public String getText(){
+        return m_texte;
+    }
+
+    public void deleteText(){
+        m_texte = "";
+    }
 }
